cb8058d43fed2bce55bef5e7f4fec6a892ba2955
Theino <theino8@yahoo.com>
2017-10-17 18:26:32 -0400

    shoots dragon boars

--- Assets/Shoot.cs
+++ Assets/Shoot.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Shoot : MonoBehaviour {
 
+    public GameObject ThingToShoot;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +13,12 @@ public class Shoot : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(Input.GetButtonDown("Fire1"))
+        {
+            Vector3 start = transform.position;
+            start += transform.forward.normalized * 10;
+            GameObject newObj = Instantiate<GameObject>(ThingToShoot, start, transform.rotation);
+            newObj.GetComponent<Rigidbody>().velocity = transform.forward.normalized * 30;
+        }
 	}
 }
