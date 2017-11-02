4e398c265cf0c1b42072c9e4ed4b1eb4946b9740
Theino <theino8@yahoo.com>
2017-11-02 13:33:04 -0400

    adding global score

---
+++ Assets/GlobalVariables.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets
+{
+    static class GlobalVariables
+    {
+        public static int Score;
+    }
+}
